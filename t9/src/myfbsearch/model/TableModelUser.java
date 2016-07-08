2b21eb00b6fe27561fc4d888b3796d3a3b7ea10f
nazzog <tialvesm@gmail.com>
2016-07-08 01:22:30 -0300

    classe TableModeUser atualizada

--- t9/src/myfbsearch/model/TableModelUser.java
+++ t9/src/myfbsearch/model/TableModelUser.java
@@ -1,5 +1,50 @@
 package myfbsearch.model;
 
-public class TableModelUser {
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TableModelUser extends AbstractTableModel {
+	
+	private static final String[] columnNames = {"pos","picture","id","name"};
+	private ArrayList<User> users;
+	
+	public TableModelUser(){
+		
+	}
+	
+	public void save(){
+		
+	}
+	
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return users.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return columnNames.length;
+	}
+	
+	@Override
+    public String getColumnName(int columnIndex) {
+        return columnNames[columnIndex];
+    }
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		switch(columnIndex){
+			case 0: users.get(rowIndex).getPos();
+			case 1: users.get(rowIndex).getPicture();
+			case 2: users.get(rowIndex).getId();
+			case 3: users.get(rowIndex).getName();
+		}
+		return null;
+	}
 
 }
